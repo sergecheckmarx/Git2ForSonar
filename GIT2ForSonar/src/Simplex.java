2491ed88b3210757b9003fe954045aec4692176d
sergei <serge.ingber@checkmarx.com>
2015-08-09 17:59:14 +0300

    modif

--- GIT2ForSonar/src/Simplex.java
+++ GIT2ForSonar/src/Simplex.java
@@ -1,10 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class Simplex {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		System.out.println("hello simplex");
 		System.out.println("how are u?");
+		// TODO Auto-generated method stub
+				BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+				System.out.println("Hello World");
+				System.out.println("Enter confidential information:");
+				String input = br.readLine();
+				String [] s = args;
 	}
 
 }
